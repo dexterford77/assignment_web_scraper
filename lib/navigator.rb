ffbd3db88ea4b94d77d939f788a980ab498849b4
James Harris <computersherpa@gmail.com>
2016-11-14 16:35:41 -0600

    Navigator handles title and location

--- lib/navigator.rb
+++ lib/navigator.rb
@@ -1,2 +1,15 @@
 class Navigator
+  attr_reader :location, :title
+  def initialize(options={})
+
+    @title = encode(options[:title])
+    @location = encode(options[:location])
+  end
+  def encode(string)
+    return "" if string == nil
+    string.gsub(' ','+').gsub(',','%2C')
+  end
+  def url
+    "https://www.dice.com/jobs?q=#{@title}&l=#{@location}"
+  end
 end
--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -3,10 +3,22 @@ require 'navigator'
 
 describe Navigator do
 
-  describe "#something" do
+  describe "#new" do
 
-    it "does stuff" do
+    it "receives a hash of options and returns a URL" do
+      p options = {title: "Engineer"}
+      engie = Navigator.new(options)
+      expect(engie.url).to start_with("https://www.dice.com/jobs?q=Engineer")
+    end
 
+    it "converts a city & state pair into a URL-compatible format" do
+      ny = Navigator.new({location: "New York, NY"})
+      expect(ny.location).to eq("New+York%2C+NY")
+    end
+
+    it "handles both a title and a location" do
+      nav = Navigator.new({title: "Engineer", location: "New York, NY"})
+      expect(nav.url).to eq("https://www.dice.com/jobs?q=Engineer&l=New+York%2C+NY")
     end
 
   end
