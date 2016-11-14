623d005d51061f48376ec2a6d97177487f3cd466
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-14 17:54:36 -0600

    Working on parsing mechanize objects

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'guard-rspec'
+gem 'mechanize', '~>2.7.2', '<2.7.3'
+gem 'pry'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,30 @@
+require_relative 'ui'
+require 'mechanize'
+
 class Scraper
 
   def initialize
-    @nav = Navigator.new
+    @ui = Ui.new
   end
 
   def run
-
+    @ui.get_input
+    @nav = Navigator.new({title: @ui.title, location: @ui.location})
+    scrape(@nav.url)
   end
 
-  def scrape
-
+  def scrape(url)
+    agent = Mechanize.new
+    # this gives your Mechanize object a 0.5 second wait time after every HTML request
+    agent.history_added = Proc.new { sleep 0.5 }
+    page = agent.get(url)
+    page = page.links_with(href: /https:\/\/www.dice.com\/jobs\/detail/).map do |link| 
+      link = [link.text.strip, link.uri]
+    end
+    puts page
   end
 
 end
+
+scraper = Scraper.new
+scraper.scrape("https://www.dice.com/jobs?q=web+developer&l=&searchid=6432325194502")
